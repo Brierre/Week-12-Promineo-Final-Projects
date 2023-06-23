14de6af90e7d9ba35780c394ccf16f077eef424a
Brierre <hlmbugle@gmail.com>
2023-06-23 19:49:51 -0400

    modal, filter, navbar, styling updates

--- promineoFinals.js
+++ promineoFinals.js
@@ -13,12 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
     drawTable();
 });
 
-function drawTable() { //function to redraw table contents upon any updates or deletions, as well as at the start of the program
+const tableDataElem = document.querySelector('#projects'); //#projects is <tbody> id
+const dataArray = [];
+
+//GET METHODS
+function drawTable() {
     $.get(URL_ENDPOINT).then(data => {
-        console.log(data);
-        $('td').remove();
-        data.map(project => { //we are going to append each project to our table, so build table in html file
-            $('#projectsList').append(
+        $('td').remove(); //clear all <td> info
+        data.map(project => { //append each project to our table, so build table in html file
+            $('#projects').append( //append rows to <tbody>
                 $(`
                 <tr>
                     <td>${project.id}</td>
@@ -39,22 +42,30 @@ function drawTable() { //function to redraw table contents upon any updates or d
 }
 
 
-$('#addForm').click(function(e) {
+//POST METHODS
+//modal popup for adding projects
+function addProject(e) {
     e.preventDefault();
     $.post(URL_ENDPOINT, { //two args, the URL_ENDPOINT and the object we want to get data for
         fullName: $('#fullName').val(),
         finalProject: $('#project').val(),
         github: $('#github').val(),
-        jdbcOrJpa: $("[name='optradio']:checked").val(), //lists in table as "ON"
-        projectCriteriaMet: $("[name='optradio2']:checked").val(), //lists in table as "ON"
+        jdbcOrJpa: $("[name='optradio']:checked").val(),
+        projectCriteriaMet: $("[name='optradio2']:checked").val(),
         videoLink: $('#video').val()
-    })
-    drawTable();
-    clearForm();
-});
+    }).then (drawTable)
+    .then($("#addModal").modal("hide"));
+}
+
+$('#addForm').on('submit', (e) => {
+    addProject(e);
+    document.getElementById('addForm').reset();
+}); //button adds new project according to input on form
 
 
-$('#updateInfo').click(function(e) {
+//PUT METHODS
+//modal popup for updating projects
+function updateProject(e) {
     e.preventDefault();
     let id = $('#idSelection').val()
     $.ajax(`${URL_ENDPOINT}/${id}`, {
@@ -63,36 +74,114 @@ $('#updateInfo').click(function(e) {
             fullName: $('#updateName').val(),
             finalProject: $('#updateProject').val(),
             github: $('#updateGithub').val(),
-            jdbcOrJpa: $("[name='optradio']:checked").val(), //lists in table as "ON"
-            projectCriteriaMet: $("[name='optradio2']:checked").val(), //lists in table as "ON"
+            jdbcOrJpa: $("[name='optradio']:checked").val(),
+            projectCriteriaMet: $("[name='optradio2']:checked").val(),
             videoLink: $('#updateVideo').val()
         }
-    }).then(drawTable);
-    clearForm();
-}); //button updates project according to information input on form
+    }).then(drawTable)
+    .then($("#updateModal").modal("hide")); 
 
+}
+    
+$('#updateForm').on('submit', (e) => {
+        updateProject(e);
+        document.getElementById('updateForm').reset();
+}); //button updates project according to input on form
+
+
+//DELETE METHODS
 function deleteProject(id) {
     $.ajax(`${URL_ENDPOINT}/${id}`, {
         method: 'DELETE'
     }).then(drawTable);
 }
 
-function clearForm() {
-    $('#fullName').val(''),
-    $('#project').val(''),
-    $('#github').val(''),
-    $('#video').val(''),
-    $('#idSelection').val(''),
-    $('#updateName').val(''),
-    $('#updateProject').val(''),
-    $('#updateGithub').val(''),
-    $('#updateVideo').val('')
-    $('#jdbcradio').prop('checked', false);
-    $('#jparadio').prop('checked', false);
-    $('#yesradio').prop('checked', false);
-    $('#noradio').prop('checked', false);
-    $('#jdbcradio2').prop('checked', false);
-    $('#jparadio2').prop('checked', false);
-    $('#yesradio2').prop('checked', false);
-    $('#noradio2').prop('checked', false);
+
+//SEARCH FILTER METHODS
+let matchingIDs = []; //store matching record IDs
+function createFilteredTable(matchingIDs) {
+    tableDataElem.innerHTML = "";
+    let searchIndex = 0;
+    let tableInd = 1;
+    $.get(URL_ENDPOINT).then(data => { //****putting data into a standard array for later use. IS THIS EVEN NECESSARY or can I just use the table itself later?****
+        data.forEach(item => {
+        dataArray.push(item);
+        });
+        console.log(dataArray[0]);
+    });    
+
+    if (typeof matchingIDs === "undefined") {
+        let i = 0;
+        while(i < Object.keys(dataArray).length){
+            tableDataElem.innerHTML += `
+                <tr>
+                    <td>${tableInd}</td>
+                    <td>${dataArray[tableInd][0]}</td>
+                    <td>${dataArray[tableInd][1]}</td>
+                    <td>${dataArray[tableInd][2]}</td>
+                    <td>${dataArray[tableInd][3]}</td>
+                    <td>${dataArray[tableInd][4]}</td>
+                    <td>${dataArray[tableInd][5]}</td>
+                    <td>${dataArray[tableInd][6]}</td>
+                    <td>
+                    <button id="delete" onclick="deleteProject(${project.id})">🗑</button>
+                    </td>
+                </tr>
+            `;
+            i++;
+            tableInd++;
+        }
+    } else if (matchingIDs.length !== 0) {
+        while(searchIndex < matchingIDs.length){ //append data to table as long as the search term matches something in the row
+            tableDataElem.innerHTML+=`
+                <tr>
+                    <td>${tableInd}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][0]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][1]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][2]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][3]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][4]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][5]}</td>
+                    <td>${dataArray[matchingIDs[searchIndex]][6]}</td>
+                    <td>
+                    <button id="delete" onclick="deleteProject(${project.id})">🗑</button>
+                    </td>
+                </tr>
+            `;
+            searchIndex++;
+            tableInd++;
+        }
+    } else { 
+        tableDataElem.innerHTML+=`
+            <tr>
+                <td colspan="5">
+                    NO DATA FOUND
+                </td>
+            </tr>
+        `;
+    }
 }
+
+//attach event listener
+document.querySelector('#searchText').addEventListener('input', function() {
+    let value = this.value.trim(); //store the search query
+    if (value) { //check if value is not empty
+        let tr = 1;
+
+        while (tr < Object.keys(dataArray).length) { //loop through the data to find matching text
+            if (dataArray[tr][0].includes(value) || //check if current property contains the search query
+                dataArray[tr][1].includes(value) ||
+                dataArray[tr][2].includes(value) ||
+                dataArray[tr][3].includes(value) ||
+                dataArray[tr][4].includes(value) ||
+                dataArray[tr][5].includes(value) ||
+                dataArray[tr][6].includes(value)) {
+                    matchingIDs.push(tr);
+            }
+            tr++;
+        }
+        createFilteredTable(matchingIDs);
+    } else {
+        drawTable();
+    }
+});
